70338891b1dddaa41cb336ccf071765833e27787
ziaurrehmanrehmanii <bc210207630@vu.edu.pk>
2022-12-31 02:47:21 +0500

    New commit With Post

--- functions/api.js
+++ functions/api.js
@@ -5,11 +5,11 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ message: "Hello World Get" });
 });
-// router.post("/", (req, res) => {
-//   res.json({ message: "Hello World" });
-// });
+router.post("/", (req, res) => {
+  res.json({ message: "Hello World Post" });
+});
 
 app.use("/.netlify/functions/api", router);
 module.exports.handler = serveless(app);
